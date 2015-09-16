78bb89c3916b2e7d5868d98c9fa5f8f0879af25e
Sidney Andrews <sidney@opsgility.com>
2015-09-16 09:48:54 -0400

    Updated project

--- SampleApp.Web/Controllers/ValuesController.cs
+++ SampleApp.Web/Controllers/ValuesController.cs
@@ -13,14 +13,14 @@ namespace SampleApp.Web.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value_1", "value_2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return $"value{id}";
+            return $"value_{id}";
         }
 
         // POST api/values
